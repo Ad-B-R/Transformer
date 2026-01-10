8868dd16d00c18b20bab45cb35555e9fea5affb5
Ad-B-R <186320933+Ad-B-R@users.noreply.github.com>
2026-01-10 23:16:45 +0530

    added layernorm adn ffn

--- model.py
+++ model.py
@@ -27,6 +27,35 @@ class PositionalEncoding(nn.Module):
         # apply trig, shape (seq_len, d_model)
         pe[:,0::2] = torch.sin(position*div_term)
         pe[:,1::2] = torch.cos(position*div_term)
+        # [1, seq_len, d_model]
         pe = pe.unsqueeze(0)
 
         self.register_buffer('pe', pe)
+
+    @torch.no_grad()
+    def forward(self, x):
+        x = x + self.pe[:, :x.shape[1], :] # for different broadcasting purposes
+        return self.dropout(x)
+class LayerNorm(nn.Module):
+    def __init__(self, eps: float = 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.gamma = nn.parameter(torch.ones(1))
+        self.beta = nn.parameter(torch.ones(1))
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.gamma * (x-mean)/(std + self.eps) + self.beta
+
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, d_model: int, d_ff:int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.fnn1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.fnn2 = nn.Linear(d_ff, d_model)
+    
+    def forward(self, x):
+        return self.fnn2(self.dropout(torch.relu(self.fnn1(x))))
