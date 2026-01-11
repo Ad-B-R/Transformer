ab6e7a06b6e6adfaeb32fc00f47414e2b7c086cf
Ad-B-R <186320933+Ad-B-R@users.noreply.github.com>
2026-01-11 12:40:34 +0530

    attetnion

--- model.py
+++ model.py
@@ -59,3 +59,47 @@ class FeedForwardNetwork(nn.Module):
     
     def forward(self, x):
         return self.fnn2(self.dropout(torch.relu(self.fnn1(x))))
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, h:int, dropout:int):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = dropout
+        assert d_model%h == 0,  "d_model not divisible by h"
+
+        self.d_k = d_model//h
+        self.Wq = nn.Linear(d_model, d_model)
+        self.Wk = nn.Linear(d_model, d_model)
+        self.Wv = nn.Linear(d_model, d_model)
+        
+        self.Wo = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+    
+    @staticmethod
+    def attention(self, query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (batch, seq_len, d_k) -> (batch, seq_len, seq_len)
+        attention_scores = ((query @ key.transpose(-2,-1))/torch.sqrt(d_k))
+        if mask is not None:
+            attention_scores.masked_fill_(mask==0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # batch, h, seq_len, seq_len
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ key), attention_scores
+
+
+    def forward(self, k, q, v, mask):
+        # batch x seq len x d_model
+        query = self.Wq(q)
+        value = self.Wq(v)
+        key = self.Wq(k)
+        # batch, seq_len, h, d_k -> batch, h, seq_len, d_k
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose([1,2])
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose([1,2])
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose([1,2])
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        
