c4c9a4f791113131049c53dfa343ff71b66bb1f8
Ad-B-R <186320933+Ad-B-R@users.noreply.github.com>
2026-01-18 23:48:44 +0530

    finished dataset loading

--- dataset.py
+++ dataset.py
@@ -25,3 +25,55 @@ class BilungialDataset(Dataset):
         src_text = src_target_pair['translation'][self.src_lang]
         target_text = src_target_pair['translation'][self.target_lang]
         
+        enc_input_token = self.tokenizer_src.encode(src_text).ids
+        dec_input_token = self.tokenizer_src.decode(src_text).ids
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_token) - 2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_token) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+        
+        # Adding sos and eos to the source text
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_token, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]*enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+        # No eos for decoder input, we only add sos
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_token, dtype=torch.int64),
+                torch.tensor([self.pad_token]*dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+        # Add sos to decoder output since this is what we expect the output to be
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_token, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]*dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+        
+        return {
+            'encoder_input': encoder_input,
+            'decoder_input': decoder_input,
+            'encoder_mask': (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),
+            'decoder_mask': (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & casual_mask(
+                decoder_input.size(0)),
+            'label': label,
+            "src_target": src_text,
+            "target_target": target_text
+        }
+def casual_mask(size):
+    mask = torch.triu(torch.ones(1,size,size), diagonal=1).type(torch.int)
+    return mask==0
--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from tokenizers.pre_tokenizers import Whitespace
 
 from pathlib import Path
 
+from dataset import BilungialDataset, casual_mask
+
 def get_all_sentences(dataset, lang):
     for item in dataset:
         yield item['translation'][lang]
@@ -33,13 +35,35 @@ def get_or_build_tokenizer(config, dataset, lang):
     return tokenizer
 
 def get_dataset(config):
-    dataset_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_target"]}', 
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_target"]}', 
                             split="train")
     
     # build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, dataset_raw, config['lang_src'])
-    tokenizer_target = get_or_build_tokenizer(config, dataset_raw, config['lang_target'])
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_target = get_or_build_tokenizer(config, ds_raw, config['lang_target'])
 
-    train_ds_size = int(0.9*len(dataset_raw))
-    val_ds_size = len(dataset_raw) - train_ds_size
-    train_ds_raw, val_ds_raw = random_split(dataset_raw, [train_ds_size, val_ds_size])
+    train_ds_size = int(0.9*len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilungialDataset(train_ds_raw, tokenizer_src, tokenizer_target, 
+                                config['lang_src'], config['lang_target'], config['seq_len'])
+    val_ds = BilungialDataset(val_ds_raw, tokenizer_src, tokenizer_target, 
+                                config['lang_src'], config['lang_target'], config['seq_len'])
+    
+    max_len_src = 0
+    max_len_target = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        target_ids = tokenizer_src.decode(item['translation'][config['lang_target']]).ids
+        max_len_src = max(max_len_src, src_ids)
+        max_len_target = max(max_len_target, target_ids)
+    
+    print(f"Max length of source sequence: {max_len_src}")
+    print(f"Max length of target sequence: {max_len_target}")
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_target
